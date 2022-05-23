3e47a6e80b17dba14718ee5f3cda93d076f9191c
samantha-hong <96512457+samantha-hong@users.noreply.github.com>
2022-05-23 13:51:06 -0700

    added studio ghibli api

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+    const url = `https://ghibliapi.herokuapp.com/films`
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        random = Math.floor(Math.random() * data.length)
+        console.log(random);
+        document.querySelector('#poster').src = data[random].image;
+        document.querySelector('#title').innerText = data[random].title;
+        document.querySelector('#time').innerText = data[random].running_time;
+        document.querySelector('#score').innerText = data[random].rt_score;
+        document.querySelector('#synopsis').innerText = data[random].description;
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
